15d612e4d15070e80a878d836cb3b1f79d61952c
Masen Matthews <masenmatthews@gmail.com>
2018-08-20 12:35:42 -0700

    add seperate homepage, delete unneccesary files

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_15_162456) do
+ActiveRecord::Schema.define(version: 2018_08_20_193233) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "homes", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.datetime "created_at", null: false
